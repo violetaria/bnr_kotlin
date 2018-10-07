694faed68ae4a4b4bc7f2b63269ede443f8acb93
Terri Chu <terri.chu.dev@gmail.com>
2018-10-07 15:32:10 -0400

    chapter 8

--- NyetHack/src/Tavern.kt
+++ NyetHack/src/Tavern.kt
@@ -1,9 +1,24 @@
+import kotlin.math.roundToInt
+
 const val TAVERN_NAME = "Jojo's Drinks and Dregs"
+const val DRAGON_COIN_TO_GOLD_CONVERSION = 1.43
+
+//var playerGold = 10
+//var playerSilver = 10
+// 1.43 gold per dragon coin
+var playerDragonCoin = 5.0
+
+var caskGallonsTotal: Double = 5.0
+const val PINT = 0.125
 
 fun main(args: Array<String>) {
-//    placeOrder("shandy,Dragon's Breath,5.91")
-    placeOrder("shandy,DRAGON'S BREATH,5.91")
+    placeOrder("shandy,Dragon's Breath,5.91")
+    placeOrder("shandy,Dragon's Breath,5.91")
+    placeOrder("shandy,Dragon's Breath,5.91")
+//    placeOrder("shandy,DRAGON'S BREATH,5.91")
 //    placeOrder("exlixir,Shirley's Temple,4.12")
+
+
 }
 
 fun placeOrder(menuData: String) {
@@ -16,16 +31,27 @@ fun placeOrder(menuData: String) {
 //    val name = data[1]
 //    val price = data[2]
 
-    val(type, name, price) = menuData.split(',')
-    val message = "Madrigal buys a $name ($type) for $price."
-    println(message)
+    val (type, name, price) = menuData.split(',')
 
-    val phrase = if (name.equals("Dragon's Breath", true)) {
-        "Madigral exclaims: ${toDragonSpeak("Ah, delicious $name!")}"
+    val purchaseSuccessful = performPurchase(price.toDouble())
+
+    if (purchaseSuccessful) {
+        val message = "Madrigal buys a $name ($type) for $price."
+        println(message)
+
+        if (name.equals("Dragon's Breath", ignoreCase = true)) {
+            pourPint()
+        }
+
+        val phrase = if (name.equals("Dragon's Breath", true)) {
+            "Madigral exclaims: ${toDragonSpeak("Ah, delicious $name!")}"
+        } else {
+            "Madrigal says: Thanks for the $name."
+        }
+        println(phrase)
     } else {
-        "Madrigal says: Thanks for the $name."
+        println("$tavernMaster says: You too poor son!")
     }
-    println(phrase)
 }
 
 fun toDragonSpeak(phrase: String) =
@@ -39,3 +65,34 @@ fun toDragonSpeak(phrase: String) =
             else -> it.value
         }
     }
+
+fun performPurchase(price: Double): Boolean {
+    displayBalance()
+    val totalPurse = playerDragonCoin * DRAGON_COIN_TO_GOLD_CONVERSION
+    println("Total purse: $totalPurse")
+    if (totalPurse - price < 0) {
+        return false
+    }
+    println("Purchasing item for $price")
+
+    val remainingBalance = totalPurse - price
+    println("Remaining balance: ${"%.2f".format(remainingBalance)}")
+
+//    playerSilver = (remainingBalance % 1 * 100.0).roundToInt()
+//    playerGold = remainingBalance.toInt()
+    playerDragonCoin = (remainingBalance / DRAGON_COIN_TO_GOLD_CONVERSION)
+    displayBalance()
+    return true
+}
+
+private fun displayBalance() {
+//    println("Player's purse balance: Gold $playerGold, Silver: $playerSilver")
+    println("Player's purse balance: Dragon Coin $playerDragonCoin")
+}
+
+fun pourPint() {
+    caskGallonsTotal -= PINT
+    println("Cask is down to $caskGallonsTotal")
+
+    println("${(caskGallonsTotal / PINT).toInt()} pints left")
+}
